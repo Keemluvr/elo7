446c1c149ebb7c9cae58cabadac96608f964eb14
Keila Fernandes <keiladaianefernandes24@gmail.com>
2024-06-29 02:16:46 -0300

    feat: add jest config

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom", "<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "/__tests__/components/.*.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@styles/(.*)$": "<rootDir>/src/styles/$1",
+  },
+};
+
+export default createJestConfig(config);
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "src", "styles")],
     prependData: `@import "@styles/main.scss";`,
