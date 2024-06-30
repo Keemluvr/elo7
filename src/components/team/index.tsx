f767c55144dd9f184703efbbc5c2a9d953d71f7a
Keila Fernandes <keiladaianefernandes24@gmail.com>
2024-06-30 02:59:17 -0300

    fix: add missing alt texts

--- src/components/team/index.tsx
+++ src/components/team/index.tsx
@@ -5,7 +5,7 @@ import "./style.scss";
 
 export default function Team() {
   return (
-    <section>
+    <section aria-label="seção da apresentação do time">
       <div className="grid-container">
         <h2 className="grid-item-full-width team-title">
           Conheça nosso time fora de série
@@ -13,14 +13,20 @@ export default function Team() {
         <div className="grid-item-full-width team-content">
           <TeamMemberCard
             name="ana"
-            alt="Membro da equipe sorrindo enquanto trabalha em um ambiente colaborativo."
+            alt="Ana, uma mulher sorrindo enquanto trabalha em um ambiente colaborativo."
           />
-          <TeamMemberCard name="joao" alt="" />
+          <TeamMemberCard
+            name="joao"
+            alt="João, um homem de barba ruiva e camisa branca, sorrindo com os braços cruzados em um ambiente de escritório, com duas pessoas ao fundo conversando."
+          />
           <TeamMemberCard
             name="maria"
-            alt="Membro da equipe sorrindo enquanto segura um laptop em um ambiente de escritório moderno."
+            alt="Maria, uma mulher sorrindo enquanto segura um laptop em um ambiente de escritório moderno."
           />
-          <TeamMemberCard name="pedro" alt="" />
+          <TeamMemberCard
+            name="pedro"
+            alt="Pedro, um homem com cabelo cacheado e barba, usando fones de ouvido e uma camisa azul, sentado em frente a um laptop e sorrindo."
+          />
         </div>
       </div>
     </section>
