76e96900151fc1f6af67a8d8455515af97cb16a5
Keila Fernandes <keiladaianefernandes24@gmail.com>
2024-06-30 02:21:58 -0300

    improve: aria props

--- src/components/saleswoman-review/index.tsx
+++ src/components/saleswoman-review/index.tsx
@@ -5,12 +5,16 @@ import "./style.scss";
 
 export default function SaleswomanReview() {
   return (
-    <section className="grid-container saleswoman-review-wrapper">
+    <section
+      className="grid-container saleswoman-review-wrapper"
+      aria-label="seção da palavra da vendedora"
+    >
       <div className="grid-item-full-width saleswoman-review-content">
         <div className="saleswoman-review-image-wrapper">
           <Image
             className="saleswoman-review-image"
             src="/images/saleswoman.webp"
+            aria-label="imagem da vendedora"
             alt="Uma mulher jovem trabalha em uma oficina de artesanato, concentrada em suas atividades. A mesa está repleta de ferramentas e materiais de trabalho, destacando um ambiente criativo e artesanal."
             quality={100}
             loading="lazy"
@@ -18,7 +22,7 @@ export default function SaleswomanReview() {
           />
         </div>
         <div className="saleswoman-review-description">
-          <hgroup>
+          <hgroup aria-label="cabecalho da avaliacao da vendedora">
             <h2 className="saleswoman-review-title">Palavra da vendedora</h2>
             <p className="saleswoman-review-subtitle">Sed rutrum condimentum</p>
           </hgroup>
