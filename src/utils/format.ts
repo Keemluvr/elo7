8f201352a5c4c0f32404eb17d241ac3b016e8b02
Keila Fernandes <keiladaianefernandes24@gmail.com>
2024-07-04 18:07:05 -0300

    docs: add js docs to the utils

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,6 +1,18 @@
+/**
+ * Capitalizes the first letter of the input string and converts the rest to lowercase.
+ *
+ * @param {string} string - The input string to be capitalized.
+ * @return {string} The capitalized string.
+ */
 export const capitalize = (string: string) =>
   string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 
+/**
+ * Capitalizes each word in a string, including words separated by hyphens.
+ *
+ * @param {string} string - The input string to be capitalized.
+ * @return {string} The capitalized string with each word capitalized and words separated by hyphens preserved.
+ */
 export const capitalizeWords = (string: string) => {
   return string
     .split(" ")
@@ -14,11 +26,23 @@ export const capitalizeWords = (string: string) => {
     .join(" ");
 };
 
+/**
+ * Formats a location string into a city, state, and country.
+ *
+ * @param {string} location - The location string to format.
+ * @return {string} The formatted location string.
+ */
 export const formatLocation = (location: string) => {
   const [city, state, country] = location.split(", ");
   return `${capitalizeWords(city)}, ${capitalizeWords(country)}`;
 };
 
+/**
+ * Replaces specific substrings in a given string with their corresponding uppercase versions.
+ *
+ * @param {string} string - The input string to be processed.
+ * @return {string} The modified string with the special cases replaced.
+ */
 export const specialCases = (string: string) => {
   return string
     .replace(/fp&a/gi, "FP&A")
